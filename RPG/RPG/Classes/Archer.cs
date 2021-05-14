eea4e9365b506ac8ad3281bbb67cc07a5157c0b3
Aleksandar Tenev <alexsandartenev@gmail.com>
2021-05-14 14:05:40 +0300

    edit 2

--- RPG/RPG/Classes/Archer.cs
+++ RPG/RPG/Classes/Archer.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using RPG.Interfaces;
 
 namespace RPG.Classes
 {
-    class Archer
+    public abstract class Archer : ICreatures
     {
+        public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double Attack { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double Defence { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double Speed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double HealthPoints { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public double Mana { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
     }
 }
--- RPG/RPG/Interfaces/ICreatures.cs
+++ RPG/RPG/Interfaces/ICreatures.cs
@@ -8,5 +8,12 @@ namespace RPG.Interfaces
 {
     interface ICreatures
     {
+        string Name { get; set; }
+        double Attack { get; set; }
+        double Defence { get; set; }
+        double Speed { get; set; }
+        double HealthPoints { get; set; }
+        double Mana { get; set; }
+       
     }
 }
