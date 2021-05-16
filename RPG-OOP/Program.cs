1f987ec6e92694dc520572ecefcc4dcb13dffd68
imp111 <alexsandartenev@gmail.com>
2021-05-16 15:54:49 +0300

    main edit

--- RPG-OOP/Program.cs
+++ RPG-OOP/Program.cs
@@ -10,18 +10,7 @@ namespace RPG_OOP
     {
         public static void Main(string[] args)
         {
-            List<ICreature> newList = new List<ICreature>();
 
-            newList.Add(new Pyromanser("Denko the great", 150, 50, 1.50, 500, 200));
-            newList.Add(new Necromanser("Denko the second great", 130, 30, 1.25, 300, 100));
-            newList.Add(new OneHandedSwordsman("Denis the one handed swords man", 200, 100, 1.50, 500, 50));
-            newList.Add(new TwoHandedSwordsman("Denis the two handed swords man", 300, 200, 1.25, 700, 50));
-            newList.Add(new Bowman("Denko the sniper", 250, 100, 1.75, 500, 30));
-
-            foreach (ICreature var in newList)
-            {
-                Console.WriteLine($"Name: {var.Name}, Attack: {var.Attack}, Defence: {var.Defence}, Speed: {var.Speed}, HP: {var.HealthPoints}, Mana: {var.Mana}");
-            }
         }
     }
 }
