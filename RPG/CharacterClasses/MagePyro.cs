ce9aea0c63488e94a1970182ac5baaf9c276332f
GeorgeDzhambazov <supremacy13@abv.bg>
2021-05-18 17:53:47 +0300

    Added all character classes

---
+++ RPG/CharacterClasses/MagePyro.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RPG.Interfaces;
+
+namespace RPG.Classes
+{
+    public abstract class Pyromancer : ICreature
+    {
+        public abstract int HealthPoints { get; }
+        public abstract int Attack { get; }
+        public abstract int Defence { get; }
+        public abstract double Speed { get; }
+        public abstract int Mana { get; }
+        public abstract string Name { get; }
+    }
